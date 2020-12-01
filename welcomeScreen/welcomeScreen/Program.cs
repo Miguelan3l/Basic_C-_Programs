e3bea3f6a932378d3544e9aad37926509638840c
Miguelan3l <miguelangel.montelongo.7@gmail.com>
2020-11-30 20:08:58 -0700

    Welcome Screen

---
+++ welcomeScreen/welcomeScreen/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("Welcome to Acme Accounting Systems\nRemember, We're \"accounting \" on you!");
+
+        Console.ReadLine();
+        }
+}
+
