93bb88f26368a96fb220cf2ec41e8b61016b2b8e
Miguelan3l <miguelangel.montelongo.7@gmail.com>
2020-12-08 20:11:41 -0700

    completed assiment

--- basic approval program for car insurance/Program.cs
+++ basic approval program for car insurance/Program.cs
@@ -7,15 +7,20 @@ namespace basic_approval_program_for_car_insurance
         static void Main(string[] args)
         {
             int age;
-            int DUI;
+            bool DUI;
             int Tick;
+            bool qual;
 
             Console.WriteLine("What is your age");
-            age = Convert.ToInt32((Console.ReadLine()));
-            Console.WriteLine("Have you ever had a DUI?");
-            DUI = ; 
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? Please answer true or false.");
+            DUI = Convert.ToBoolean(Console.ReadLine());
             Console.WriteLine("How many speeding tickets do you have?");
-            Tick = Convert.ToInt32((Console.ReadLine()));
+            Tick = Convert.ToInt32(Console.ReadLine());
+
+             qual = ( age >= 15 && ( DUI = false ) || Tick <= 3 );
+            Console.WriteLine("Qualified? " + qual);
+
         }
     }
 }
