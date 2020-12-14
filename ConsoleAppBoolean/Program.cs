87a0036a3577ba58ede9774f0cfefafc272ef8a0
Miguelan3l <miguelangel.montelongo.7@gmail.com>
2020-12-13 17:53:25 -0700

    Console app

---
+++ ConsoleAppBoolean/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleAppBoolean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///Do a boolean comparison using a while statement.
+            Console.WriteLine("Hello World!, This programs helps you count to TEN ");
+            int i = 0; // initialization
+
+            while (i < 11) 
+            {
+                Console.WriteLine("Number: = {0}", i);
+                System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+                
+                i++;
+            }
+
+            //Do a boolean comparison using a do while statement.
+
+            Console.WriteLine("Hello World! , This program helps you with your multiplication by 10!  ");
+            int table = 10;
+            int s = 1;
+            int result;
+
+            do
+            {
+                result = table * s;
+                Console.WriteLine("{0}*{1}={2}", table, s, result);
+                s++;
+            }
+            while (s <= 10);
+            Console.ReadLine(); 
+
+
+        }
+    }
+}
